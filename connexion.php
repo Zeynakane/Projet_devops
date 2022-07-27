0745b176d5604bd47cb862733a8e86b833cdc75f
Zeynakane <99742019+Zeynakane@users.noreply.github.com>
2022-07-27 23:02:40 +0000

    Add files via upload

---
+++ connexion.php
@@ -0,0 +1,7 @@
+<?php
+
+$connect = mysqli_connect("databases","groupe3","groupe3","groupe3");
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+?>
