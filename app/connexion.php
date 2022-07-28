7044bde93b74f4b3a315ad4b53594af38a052ea7
Zeynakane <99742019+Zeynakane@users.noreply.github.com>
2022-07-28 17:46:59 +0000

    Update connexion.php

--- app/connexion.php
+++ app/connexion.php
@@ -1,6 +1,6 @@
 <?php
 
-$connect = mysqli_connect("BDD_gestion_employes","groupe3","groupe3","groupe3");
+$connect = mysqli_connect("bdd_gestion_employes","groupe3","groupe3","groupe3");
 if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
